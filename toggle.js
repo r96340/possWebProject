f64ee36ddc53a151b96739cae7f0f12505a156b1
Joseph Ke <r96340@gmail.com>
2021-04-06 09:59:54 +0800

    Upload missed JavaScript file
    Version Beta 1.0 Patch 210406

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function toggle() {
+    let x = document.getElementById("bio");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
